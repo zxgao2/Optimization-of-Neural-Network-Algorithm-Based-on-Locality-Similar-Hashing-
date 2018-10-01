51a0a1e148f65b06bb639dcbd775233fba15b744
zxgao2 <39103082+zxgao2@users.noreply.github.com>
2018-10-01 15:26:17 -0700

    Add files via upload

---
+++ mixed/make_data.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+import libpylshbox as pylshbox
+import numpy as np
+import time
+from sklearn import datasets
+import os
+import cPickle
+import numpy
+from PIL import Image
+import commondata
+
+dirname = 'train'
+myfile = 'train.pkl'
+
+number = 0
+for filename in os.listdir(dirname):
+ if filename == '.DS_Store':
+  continue
+ else:
+    filename1 = dirname + '/' + filename
+    for filename2 in os.listdir(filename1):
+        if filename2 == '.DS_Store':
+         continue
+        else:
+         number += 1
+
+pictures = numpy.empty((number,commondata.ImageWidth*commondata.ImageHeight))
+lables = numpy.empty(number)
+
+index = 0
+lable = 0
+index1 = 0
+number1 = 0
+Data_begin = []
+Data_end = []
+for filename in os.listdir(dirname):
+ if filename == '.DS_Store' or filename == '0' or filename == '6' or filename == '7' or filename == '8' or filename == '9' : 
+  continue
+ else:
+    filename1 = dirname + '/' + filename
+    for filename2 in os.listdir(filename1):
+        if filename2 == '.DS_Store' :
+            continue
+        else:
+           filename2 = filename1 + '/' + filename2
+           image = Image.open(filename2)
+           image = image.convert('L')
+           image = image.resize((commondata.ImageHeight,commondata.ImageWidth))
+           image_narray = numpy.asarray(image,dtype = 'float64')/256
+           pictures[index] = numpy.ndarray.flatten(image_narray)
+           lables[index] = lable
+           index = index + 1
+           number1 += 1
+    float_query = pictures[index-number1];
+    float_mat = []
+    print index, number1
+    print index-number1
+    for i in range(index-number1,index):
+        float_mat.append(pictures[i])
+    rhp_mat = pylshbox.rhplsh()
+    float_mat=np.array(float_mat)
+    rhp_mat.init_mat(float_mat.tolist(), '', 521, 32, 3)
+    float_query = np.array(float_query)
+    result = rhp_mat.query(float_query.tolist(), 2, number1)
+    indices, dists = result[0], result[1]
+    k = (dists[len(indices)-1] - dists[1]) /number1
+    k1 = dists[1]
+    print(k)
+#   group=100
+ #   for j in range(group):
+  #      float_query = np.array(float_query)
+#        print ' '
+ #       print(float_query)
+#        print ' '
+ #       result = rhp_mat.query(float_query.tolist(), 2, (number1/group)+1)
+ #       indices, dists = result[0], result[1]
+  #      print(len(indices))
+ #   Data_begin.append(commondata.Data_index)
+    for i in range(len(indices)):
+        print indices[i], '\t', dists[i]
+    Data_begin.append(len(commondata.Data_index))
+    for i in range(len(indices)):
+        #commondata.Data_index.append(indices[i]+number1*lable+1)
+        #print (dists[i] - k1)
+       if i < len(indices)-1 :
+        if (dists[i] - k1) <=  k*1.05 :
+        #print (i*number1/group+j)
+          commondata.Data_index.append(indices[i]+index-number1+1)
+          k1 = dists[i]
+          continue
+        else :
+             Data_end.append(len(commondata.Data_index)-1)
+             commondata.Data_index.append(indices[i]+index-number1+1)
+             Data_begin.append(len(commondata.Data_index)-1)
+
+       else:
+            commondata.Data_index.append(indices[i]+index-number1+1)
+            Data_end.append(len(commondata.Data_index)-1)
+
+       k1 = dists[i]
+
+ #   float_query = float_mat[indices[len(indices)-1]]
+  #      print indices[len(indices)-1]
+    lable = lable + 1
+    number1 = 0;
+#for i in commondata.Data_index:
+#print (commondata.Data_index)
+#print(pictures)
+
+
+commondata.Data_begin_end =zip(Data_begin,Data_end)
+commondata.Modify_index(commondata.Data_index)
+commondata.Modify_begin(commondata.Data_begin_end)
+for i in commondata.Data_begin_end:
+ print i[0],  i[1]
+print len(commondata.Data_begin_end)
+lables = lables.astype(numpy.int)
+write_file=open(myfile,'wb')
+cPickle.dump([pictures[:],lables[:],commondata.Data_begin_end[:],commondata.Data_index[:]],write_file,-1)
+write_file.close()
---
+++ mixed/run.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import data
+import random
+training_data,test_data,begin_end,index = data.load_data_wrapper()
+
+import network
+net = network.Network([1024,30,10])
+random.shuffle(begin_end)
+mini_batches= []
+for i in range(len(begin_end)):
+    mini_batch1=[]
+    for k in range(begin_end[i][0],begin_end[i][1]+1):
+        mini_batch1.append(training_data[index[k]-1])
+    random.shuffle(mini_batch1)
+        #    if len(mini_batch1) > max_index:
+        #max_index = len(mini_batch1)
+        #mini_batches.append(mini_batch1)
+        #for i in range (max_index):
+        #mini_batch1= []
+        #for k in range(len(mini_batches)):
+        #if len(mini_batches[k]) > i :
+        #   mini_batch1.append(mini_batches[k][i])
+        #else :
+        #    mini_batch1.append(mini_batches[k][random.randint(0,len(mini_batches[k])-1)])
+    mini_batches.append(mini_batch1)
+d=15.0
+    #for i in range(10):
+    
+d=d / 10
+    #print(d)
+net.SGD(mini_batches,50,begin_end,index,d,test_data = test_data)
+
+import cPickle
+write_file = open('weight.pkl','wb')
+cPickle.dump([net.weights[:],net.biases[:]],write_file,-1)
+write_file.close()
+
